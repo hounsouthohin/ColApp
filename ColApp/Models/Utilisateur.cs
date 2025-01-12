cf2d0b52b58fd1381f57eeb161957e700c4ecdec
hounsouthohin <130397025+hounsouthohin@users.noreply.github.com>
2025-01-12 03:55:53 -0800

    changes

--- ColApp/Models/Utilisateur.cs
+++ ColApp/Models/Utilisateur.cs
@@ -18,41 +18,57 @@ namespace ColApp.Models
         [Key]
         [Column("idUtilisateur")]
         public int IdUtilisateur { get; set; }
+
         [Column("nom")]
         [StringLength(50)]
         [Unicode(false)]
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
         public string Nom { get; set; } = null!;
+
         [Column("prenom")]
         [StringLength(50)]
         [Unicode(false)]
+        [Required(ErrorMessage = "Le prénom est obligatoire.")]
         public string Prenom { get; set; } = null!;
+
         [StringLength(50)]
         [Unicode(false)]
         public string Role { get; set; } = null!;
+
         [Column("courriel")]
         [StringLength(100)]
         [Unicode(false)]
+        [Required(ErrorMessage = "Le courriel est obligatoire.")]
+        [EmailAddress(ErrorMessage = "Format du courriel invalide.")]
         public string Courriel { get; set; } = null!;
+
         [Column("motDePasse")]
         [StringLength(100)]
         [Unicode(false)]
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [MinLength(8, ErrorMessage = "Le mot de passe doit contenir au moins 8 caractères.")]
         public string MotDePasse { get; set; } = null!;
+
         [Column("sel")]
         [StringLength(100)]
         [Unicode(false)]
         public string Sel { get; set; } = null!;
+
         [Column("date_naissance")]
         [StringLength(50)]
         [Unicode(false)]
         public string? DateNaissance { get; set; }
+
         [StringLength(100)]
         [Unicode(false)]
         public string? PasswordResetToken { get; set; }
+
         [Column(TypeName = "datetime")]
         public DateTime? ResetTokenExpires { get; set; }
 
         [InverseProperty("IdUtilisateurNavigation")]
         public virtual ICollection<Message> Messages { get; set; }
+
         [InverseProperty("IdUtilisateurNavigation")]
         public virtual ICollection<PhotoUtilisateur> PhotoUtilisateurs { get; set; }
     }
--- ColApp/Services/ServiceInscription.cs
+++ ColApp/Services/ServiceInscription.cs
@@ -1,20 +1,37 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ColApp.Data;
-using ColApp.Models;
 using System.Data;
-using System.Globalization;
 using System.Security.Cryptography;
 
 namespace ColApp.Services
 {
     public class ServiceInscription
     {
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le nom ne peut pas dépasser 50 caractères.")]
         public string Nom { get; set; }
+
+        [Required(ErrorMessage = "Le prénom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le prénom ne peut pas dépasser 50 caractères.")]
         public string Prenom { get; set; }
+
+        [Required(ErrorMessage = "Le courriel est obligatoire.")]
+        [EmailAddress(ErrorMessage = "Le format du courriel n'est pas valide.")]
         public string Courriel { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Le mot de passe doit comporter au moins 8 caractères.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{6,}$",
+        ErrorMessage = "Le mot de passe doit contenir au moins une majuscule, un chiffre, un caractère spécial, et être long de 6 caractères.")]
         public string MDP { get; set; }
-        public DateTime Date_naissance { get; set; }   
+
+        [Required(ErrorMessage = "La date de naissance est obligatoire.")]
+        [DataType(DataType.Date, ErrorMessage = "La date de naissance n'est pas valide.")]
+        public DateTime Date_naissance { get; set; }
+
         private IDbContextFactory<BDEtabContext> _BDEtabContext { get; set; }
 
         public ServiceInscription(IDbContextFactory<BDEtabContext> BDEtabContext)
@@ -31,9 +48,9 @@ namespace ColApp.Services
             bool utiliser = false;
             var context = _BDEtabContext.CreateDbContextAsync().Result;
             List<string> courriel = (from Utilisateur in context.Utilisateurs
-                           where Utilisateur.Courriel == email
-                           select Utilisateur.Courriel).ToList();
-            
+                                     where Utilisateur.Courriel == email
+                                     select Utilisateur.Courriel).ToList();
+
             if (courriel.Count == 1)
             {
                 utiliser = true;
@@ -41,36 +58,29 @@ namespace ColApp.Services
             return utiliser;
         }
 
-
-
         public async Task InsererNouveauCompte(string nom, string prenom, DateTime date_naissance, string email, string MDP)
         {
             var dbContext = await _BDEtabContext.CreateDbContextAsync();
 
-            // Convertir la date en chaîne au format dd/MM/yyyy
             string dateFormattee = date_naissance.ToString("dd/MM/yyyy");
 
-            // Déclaration des paramètres avec SqlParameter
             var param1 = new SqlParameter("@nom", SqlDbType.NVarChar) { Value = nom };
             var param2 = new SqlParameter("@prenom", SqlDbType.NVarChar) { Value = prenom };
-            var param3 = new SqlParameter("@date_naissance", SqlDbType.NVarChar) { Value = dateFormattee };  // Utiliser NVARCHAR ici
+            var param3 = new SqlParameter("@date_naissance", SqlDbType.NVarChar) { Value = dateFormattee };
             var param4 = new SqlParameter("@courriel", SqlDbType.NVarChar) { Value = email };
             var param5 = new SqlParameter("@mdp", SqlDbType.NVarChar) { Value = MDP };
-            var param6 = new SqlParameter("@role", SqlDbType.NVarChar) { Value = "Utilisateur" };  // Rôle par défaut
+            var param6 = new SqlParameter("@role", SqlDbType.NVarChar) { Value = "Utilisateur" };
 
-            // Exécution de la procédure stockée avec les paramètres
             await dbContext.Database.ExecuteSqlRawAsync(
-                "EXEC ajoutUtilisateur @nom, @prenom,@role,@courriel,@date_naissance,@mdp",
+                "EXEC ajoutUtilisateur @nom, @prenom, @role, @courriel, @date_naissance, @mdp",
                 param1, param2, param6, param4, param3, param5);
         }
 
-
         public string CreateRandomToken()
         {
-            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64)); 
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
         }
-
-
     }
 }
+
 
