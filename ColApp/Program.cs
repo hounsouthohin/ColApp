0ad0da597350db5c1e1355b8f1c2fb6fc6083c71
hounsouthohin <130397025+hounsouthohin@users.noreply.github.com>
2025-01-15 01:46:35 -0800

    changes

--- ColApp/Program.cs
+++ ColApp/Program.cs
@@ -1,14 +1,13 @@
 using ColApp.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 using ColApp.Services;
 using ColApp.Authentication;
 using ColApp.Interfaces;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace ColApp
 {
@@ -18,18 +17,21 @@ namespace ColApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Configuration de la chaîne de connexion avec le mot de passe
             var conStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
             conStrBuilder.Password = builder.Configuration["MDP"];
 
             // Ajouter la configuration de l'application
             builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-            var baseUrl = builder.Configuration["AppSettings:BaseUrl"];  
+            // Récupérer le BaseUrl depuis appsettings.json (si nécessaire)
+            var baseUrl = builder.Configuration["AppSettings:BaseUrl"];
 
+            // Configuration du DbContext
             builder.Services.AddPooledDbContextFactory<BDEtabContext>(
-                x => x.UseSqlServer(conStrBuilder.ConnectionString));
+                options => options.UseSqlServer(conStrBuilder.ConnectionString));
 
-            // Add services to the container.
+            // Ajouter les services Blazor et les services personnalisés
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
@@ -37,46 +39,50 @@ namespace ColApp
             builder.Services.AddScoped<ServiceConnexion>();
             builder.Services.AddScoped<ServiceInscription>();
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-            builder.Services.AddScoped<CustomAuthenticationStateProvider>(); // Enregistrement du provider d'authentification
             builder.Services.AddSingleton<UserAccountService>();
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-           
-
-
-
-
-            // Enregistrer EmailSender dans l'injection de dépendances
             builder.Services.AddSingleton<IEmailSender, EmailSender>();
             builder.Services.AddSingleton<ITokenService, TokenService>();
-            
             builder.Services.AddSingleton<PasswordResetService>();
 
+            // **Ajouter l'authentification**
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
+            .AddCookie() // Pour les cookies d'authentification
+            .AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+                googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+                googleOptions.CallbackPath = "/signin-google"; // URI de redirection
+            });
 
-
+            // Construire l'application
             var app = builder.Build();
 
-
-          
-
-            // Configure the HTTP request pipeline.
+            // Configurer les middlewares
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            // Ajouter le middleware d'authentification
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // Configuration des routes Blazor
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
-            
 
-
+            // Lancer l'application
             app.Run();
         }
     }
